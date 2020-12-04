6c1e31cde834c4a220a279ec649ce1b1ce1ef075
Ngoc Nguyen <duyngoc.nguyen2001@gmail.com>
2020-12-04 11:36:14 +0700

    bombsquad pre init

--- core/src/com/oneeightfive/bombsquad/BombSquad.java
+++ core/src/com/oneeightfive/bombsquad/BombSquad.java
@@ -1,33 +1,38 @@
 package com.oneeightfive.bombsquad;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BombSquad extends ApplicationAdapter {
+public class BombSquad extends Game {
+	public static final int V_WIDTH = 960;
+	public static final int V_HEIGHT = 540;
+	public static final float PPM = 100;
+
 	SpriteBatch batch;
-	Texture img;
-	
+	public static AssetManager manager;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		manager = new AssetManager();
+		manager.finishLoading();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+		manager.update();
 	}
 	
 	@Override
 	public void dispose () {
+		super.dispose();
 		batch.dispose();
-		img.dispose();
+		manager.dispose();
 	}
 }
--- desktop/src/com/oneeightfive/bombsquad/desktop/DesktopLauncher.java
+++ desktop/src/com/oneeightfive/bombsquad/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.oneeightfive.bombsquad.BombSquad;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width =  BombSquad.V_WIDTH;
+		config.height = BombSquad.V_HEIGHT;
 		new LwjglApplication(new BombSquad(), config);
 	}
 }
